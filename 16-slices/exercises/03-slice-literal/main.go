ff1a027c36c5729492482595a24ea4764140eabd
ltozato <leo.muniztozato@gmail.com>
2019-10-17 11:11:09 -0300

    slice exercises part 1

--- 16-slices/exercises/03-slice-literal/main.go
+++ 16-slices/exercises/03-slice-literal/main.go
@@ -49,15 +49,18 @@ func main() {
 		alives    []bool    // Up/Down status of web servers
 	)
 
-	names = []string{}
-	distances = []int{}
-	data = []byte{}
-	ratios = []float64{}
-	alives = []bool{}
+	names = []string{"batman", "robin", "joker"}
+	distances = []int{15, 19, 42}
+	data = []byte{'A', 'B', 'C'}
+	ratios = []float64{14.2, 3.14, 49.3}
+	alives = []bool{true, false, true}
 
 	fmt.Printf("names    : %T %d %t\n", names, len(names), names == nil)
 	fmt.Printf("distances: %T %d %t\n", distances, len(distances), distances == nil)
 	fmt.Printf("data     : %T %d %t\n", data, len(data), data == nil)
 	fmt.Printf("ratios   : %T %d %t\n", ratios, len(ratios), ratios == nil)
 	fmt.Printf("alives   : %T %d %t\n", alives, len(alives), alives == nil)
+	if len(distances) == len(data) {
+		fmt.Println("The length of the distances and the data slices are the same.")
+	}
 }
